7ac22861019910ed2f1d7dfe32fff78e4e1bbe9f
lorenzo-vecchio <106695602+lorenzo-vecchio@users.noreply.github.com>
2023-06-06 12:30:40 +0200

    .

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
-from flask import Flask, make_response, request, session
-import json
+from flask import Flask, make_response, request, session, jsonify
 import mysql.connector
 
 app = Flask(__name__)
@@ -30,6 +29,27 @@ def login():
         if 'username' in session and 'user_id' in session:
             return make_response('logged in', 200)
         else:
+            return make_response('not logged', 401)
+        
+        
+@app.route("/animals", methods=['GET', 'POST'])
+def animals():
+    if request.method == 'GET':
+        if 'username' in session and 'user_id' in session:
+            cursor.execute("SELECT animali.nomeAnimale, animali.sesso, animali.data_di_nascita, razze.nomeRazza, specie.nomeSpecie FROM animali INNER JOIN razze ON animali.id_razza = razze.id INNER JOIN specie ON razze.id_specie = specie.id WHERE animali.id_utente = %s;", (session.get('user_id'),))
+            rows = cursor.fetchall()
+            animals = []
+            for row in rows:
+                animal = {
+                    'nomeAnimale': row[0],
+                    'sesso': row[1],
+                    'data_di_nascita': row[2],
+                    'nomeRazza': row[3],
+                    'nomeSpecie': row[4]
+                }
+                animals.append(animal)
+            return jsonify(animals)
+        if not ('username' in session and 'user_id' in session):
             return make_response('not logged', 401)
 
 
