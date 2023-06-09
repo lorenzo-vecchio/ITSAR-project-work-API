107d73bed74b492f1a0d662abe6c84b0cbb9f886
lorenzo-vecchio <106695602+lorenzo-vecchio@users.noreply.github.com>
2023-06-09 14:13:25 +0200

    .

--- app.py
+++ app.py
@@ -1,17 +1,29 @@
-from flask import Flask, make_response, request, session, jsonify
+from flask import Flask, make_response, request, session, jsonify, Response
+from flask_cors import CORS, cross_origin
 import mysql.connector
 
 app = Flask(__name__)
-app.secret_key = 'bellabellabella12345678910'
+app.config['SESSION_COOKIE_SECURE'] = True
+app.config['SESSION_COOKIE_HTTPONLY'] = True
+app.config['SESSION_COOKIE_SAMESITE'] = "None"
+CORS(app, supports_credentials=True, resources={r"/*": {"origins": ["http://localhost:3000"]}}, allow_headers="*")
 
-cnx = mysql.connector.connect(user='root', password='password', host='localhost', port='3306', database='project_work')
-cursor = cnx.cursor()
+app.secret_key = 'fdfgfdsgfgdsfgdfdfdfdsfdsds'
+
+
+
+@app.before_request
+def basic_authentication():
+    if request.method.lower() == 'options':
+        return Response()
 
 # 400 = bad request (username or password missing)
-# 401 = password or username not valid
+# 401 = password or username not valid / not authorized
 
 @app.route("/login", methods=['GET', 'POST'])
 def login():
+    cnx = mysql.connector.connect(user='root', password='password', host='localhost', port='3306', database='project_work')
+    cursor = cnx.cursor()
     if request.method == 'POST':
         try:
             username = request.json['username']
@@ -21,7 +33,7 @@ def login():
         cursor.execute("SELECT id, userName, email FROM utenti WHERE userName = %s AND password = %s", (username, password))
         user = cursor.fetchone()
         if user is None:
-            make_response('not valid username or password', 401)
+            return make_response('not valid username or password', 401)
         session['user_id'] = user[0]
         session['username'] = username
         return make_response('logged in', 200)
@@ -34,6 +46,8 @@ def login():
         
 @app.route("/animals", methods=['GET', 'POST'])
 def animals():
+    cnx = mysql.connector.connect(user='root', password='Lollipoplollipop02', host='localhost', port='3306', database='project_work')
+    cursor = cnx.cursor()
     if request.method == 'GET':
         if 'username' in session and 'user_id' in session:
             cursor.execute("SELECT animali.nomeAnimale, animali.sesso, animali.data_di_nascita, razze.nomeRazza, specie.nomeSpecie FROM animali INNER JOIN razze ON animali.id_razza = razze.id INNER JOIN specie ON razze.id_specie = specie.id WHERE animali.id_utente = %s;", (session.get('user_id'),))
@@ -52,6 +66,29 @@ def animals():
         if not ('username' in session and 'user_id' in session):
             return make_response('not logged', 401)
 
+@app.route("/luoghi", methods=['GET'])
+def luoghi():
+    cnx = mysql.connector.connect(user='root', password='Lollipoplollipop02', host='localhost', port='3306', database='project_work')
+    cursor = cnx.cursor()
+    if request.method == 'GET':
+        if 'username' in session and 'user_id' in session:
+            cursor.execute("SELECT l.nomeLuogo, tl.nomeTipo, loc.nomeLocalita, prov.sigla, reg.nomeRegione, l.latitudine, l.longitudine FROM luoghi AS l JOIN tipo_luoghi AS tl ON l.id_tipo_luogo = tl.id JOIN localita AS loc ON l.id_localita = loc.id JOIN province AS prov ON loc.id_provincia = prov.id JOIN regioni AS reg ON prov.id_regione = reg.id;")
+            rows = cursor.fetchall()
+            luoghi = []
+            for row in rows:
+                luogo = {
+                    'nome': row[0],
+                    'tipo': row[1],
+                    'comune': row[2],
+                    'provincia': row[3],
+                    'regione': row[4],
+                    'latitudine': row[5],
+                    'longitudine': row[6]
+                }
+                luoghi.append(luogo)
+            return jsonify(luoghi)
+        if not ('username' in session and 'user_id' in session):
+            return make_response('not logged', 401)
 
 @app.route("/logout", methods=['POST'])
 def logout():
@@ -59,4 +96,4 @@ def logout():
     return make_response('logged out', 200)
     
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False)
