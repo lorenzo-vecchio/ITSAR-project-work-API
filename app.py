c3adaefb92af0b6790a6b65b64ef5c38490fcedc
lorenzo-vecchio <106695602+lorenzo-vecchio@users.noreply.github.com>
2023-06-10 22:42:41 +0200

    added register

--- app.py
+++ app.py
@@ -27,6 +27,28 @@ def basic_authentication():
 # 400 = bad request (username or password missing)
 # 401 = password or username not valid / not authorized
 
+@app.route("/register", methods=['POST'])
+def register():
+    cnx = mysql.connector.connect(**db_connection_info)
+    cursor = cnx.cursor()
+    if request.method == 'POST':
+        try:
+            username = request.json['username']
+            password = request.json['password']
+            mail = request.json['mail']
+        except:
+            return make_response('ERROR: username or password missing', 400)
+        cursor.execute("SELECT id, userName FROM utenti WHERE userName = %s AND password = %s", (username, password))
+        user = cursor.fetchone()
+        if user is None:
+            cursor.execute("INSERT INTO utenti (userName, password, email) VALUES ('%s', '%s', '%s');", (username, password, mail))
+            user [0] = cursor.lastrowid
+            cnx.commit
+        session['user_id'] = user[0]
+        session['username'] = username
+        return make_response('logged in', 200)
+        
+
 @app.route("/login", methods=['GET', 'POST'])
 def login():
     cnx = mysql.connector.connect(**db_connection_info)
@@ -55,9 +77,9 @@ def login():
 def animals():
     cnx = mysql.connector.connect(**db_connection_info)
     cursor = cnx.cursor()
-    if request.method == 'GET':
-        if 'username' in session and 'user_id' in session:
-            cursor.execute("SELECT animali.nomeAnimale, animali.sesso, animali.data_di_nascita, razze.nomeRazza, specie.nomeSpecie FROM animali INNER JOIN razze ON animali.id_razza = razze.id INNER JOIN specie ON razze.id_specie = specie.id WHERE animali.id_utente = %s;", (session.get('user_id'),))
+    if 'username' in session and 'user_id' in session:
+        if request.method == 'GET':
+            cursor.execute("SELECT animali.nomeAnimale, animali.sesso, animali.data_di_nascita, razze.nomeRazza, specie.nomeSpecie FROM animali  INNER JOIN razze ON animali.id_razza = razze.id INNER JOIN specie ON razze.id_specie = specie.id WHERE animali.id_utente = '%s';", (session.get('user_id'),))
             rows = cursor.fetchall()
             animals = []
             for row in rows:
@@ -70,15 +92,15 @@ def animals():
                 }
                 animals.append(animal)
             return jsonify(animals)
-        if not ('username' in session and 'user_id' in session):
-            return make_response('not logged', 401)
+    if not ('username' in session and 'user_id' in session):
+        return make_response('not logged', 401)
 
 @app.route("/luoghi", methods=['GET'])
 def luoghi():
     cnx = mysql.connector.connect(**db_connection_info)
     cursor = cnx.cursor()
-    if request.method == 'GET':
-        if 'username' in session and 'user_id' in session:
+    if 'username' in session and 'user_id' in session:
+        if request.method == 'GET':
             cursor.execute("SELECT l.nomeLuogo, tl.nomeTipo, loc.nomeLocalita, prov.sigla, reg.nomeRegione, l.latitudine, l.longitudine FROM luoghi AS l JOIN tipo_luoghi AS tl ON l.id_tipo_luogo = tl.id JOIN localita AS loc ON l.id_localita = loc.id JOIN province AS prov ON loc.id_provincia = prov.id JOIN regioni AS reg ON prov.id_regione = reg.id;")
             rows = cursor.fetchall()
             luoghi = []
@@ -94,8 +116,8 @@ def luoghi():
                 }
                 luoghi.append(luogo)
             return jsonify(luoghi)
-        if not ('username' in session and 'user_id' in session):
-            return make_response('not logged', 401)
+    if not ('username' in session and 'user_id' in session):
+        return make_response('not logged', 401)
 
 @app.route("/logout", methods=['POST'])
 def logout():
