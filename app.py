e03e09f65389aa9cca10c6da05b11d6774d71faf
lorenzo-vecchio <106695602+lorenzo-vecchio@users.noreply.github.com>
2023-06-27 11:19:43 +0200

    added get promemoria

--- app.py
+++ app.py
@@ -172,11 +172,11 @@ def preferiti():
     if 'username' in session and 'user_id' in session:
         query = """
         SELECT s.id, s.nomeLuogo, l.nomeLocalita, l.provincia
-    FROM utenti AS u
-    INNER JOIN preferiti AS p ON u.id = p.id_utente 
-    INNER JOIN servizi AS s ON p.id_servizi = s.id 
-    INNER JOIN localita AS l ON s.id_localita = l.id
-    WHERE u.id = %s;
+        FROM utenti AS u
+        INNER JOIN preferiti AS p ON u.id = p.id_utente 
+        INNER JOIN servizi AS s ON p.id_servizi = s.id 
+        INNER JOIN localita AS l ON s.id_localita = l.id
+        WHERE u.id = %s;
         """
         rows = data.execute_query(query, (session.get('user_id'),))
         luoghi_preferiti = []
@@ -189,6 +189,36 @@ def preferiti():
             }
             luoghi_preferiti.append(luogo_preferito)
         return jsonify(luoghi_preferiti)
+    return make_response('not logged', 401)
+
+@app.route("/promemoria", methods=['GET'])
+def promemoria():
+    data = DatabaseConnector(db_connection_info)
+    data.connect()
+    if 'username' in session and 'user_id' in session:
+        query = """
+        SELECT pm.id, pm.titolo, pm.descrizione, pm.data_e_ora, GROUP_CONCAT(a.nomeAnimale SEPARATOR ', ') AS animali
+        FROM promemoria AS pm
+        INNER JOIN riferimento AS r ON pm.id = r.id_promemoria
+        INNER JOIN animali AS a ON r.id_animale = a.id
+        INNER JOIN utenti AS u ON a.id_utente = u.id
+        WHERE u.id = %s AND pm.data_e_ora >= CURDATE()
+        GROUP BY pm.id, pm.titolo, pm.descrizione, pm.data_e_ora
+        ORDER BY pm.data_e_ora;
+
+        """
+        rows = data.execute_query(query, (session.get('user_id'),))
+        promemorias = []
+        for row in rows:
+            promemoria = {
+                'id': row[0],
+                'titolo': row[1],
+                'descrizione': row[2],
+                'data_ora': row[3],
+                'animali': row[4]
+            }
+            promemorias.append(promemoria)
+        return jsonify(promemorias)
     return make_response('not logged', 401)
 
 
